3f4b22eb7ca7291da5a5075cc02a4816c0b52202
kubonnt <kuba.sm.WTF@gmail.com>
2022-02-04 13:37:43 +0100

    Added a camera. Todo: separate everthing/every object in separate header files, create objects and appropriate cpp files with methods.

--- Engine/Alix_1.1/main.cpp
+++ Engine/Alix_1.1/main.cpp
@@ -14,31 +14,85 @@
 unsigned int SCR_WIDTH = 800;
 unsigned int SCR_HEIGHT = 600;
 
+glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+
+bool firstMouse = true;
+float yaw = -90.0f;
+float pitch = 0.0f;
+float lastX = 800.0f / 2.0f;
+float lastY = 600.0f / 2.0f;
+float fov = 45.0f;
+
+float deltaTime = 0.0f;
+float lastFrame = 0.0f;
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
 	glViewport(0, 0, width, height);
 }
 
-float mixValue = 0.2f; // stores how much we're seeing of either textures 
-
 void processInput(GLFWwindow* window)
 {
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, true);
 	
+	const float cameraSpeed = static_cast<float>(10.0f * deltaTime);
 	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
-	{
-		mixValue += 0.001f;
-		if (mixValue >= 1.0f)
-			mixValue = 1.0f;
-	}
+		cameraPos += cameraSpeed * cameraFront;
 	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+		cameraPos -= cameraSpeed * cameraFront;
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+		cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+		cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+}
+
+void mause_callback(GLFWwindow* window, double xposIn, double yposIn)
+{
+	float xpos = static_cast<float>(xposIn);
+	float ypos = static_cast<float>(yposIn);
+
+	if (firstMouse)
 	{
-		mixValue -= 0.001f;
-		if (mixValue <= 0.0f)
-			mixValue = 0.0f;
+		lastX = xpos;
+		lastY = ypos;
+		firstMouse = false;
 	}
+
+	float xoffset = xpos - lastX;
+	float yoffset = lastY - ypos;
+	lastX = xpos;
+	lastY = ypos;
+
+	float sensitivity = 0.5f;
+	xoffset *= sensitivity;
+	yoffset *= sensitivity;
+
+	yaw += xoffset;
+	pitch += yoffset;
+
+	// make sure that when the pitch is out of bounds the screen doestn't get flipped
+	if (pitch > 89.0f)
+		pitch = 89.0f;
+	if (pitch < -89.0f)
+		pitch = -89.0f;
+
+	glm::vec3 front;
+	front.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+	front.y = sin(glm::radians(pitch));
+	front.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+	cameraFront = glm::normalize(front);
+}
+
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+	fov -= (float)yoffset;
+	if (fov < 1.0f)
+		fov = 1.0f;
+	if (fov > 45.0f)
+		fov - 45.0f;
 }
 
 float vertices[] = {
@@ -120,6 +174,8 @@ int main()
 	}
 	glfwMakeContextCurrent(window);
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	glfwSetCursorPosCallback(window, mause_callback);
+	glfwSetScrollCallback(window, scroll_callback);
 	
 	 
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
@@ -128,12 +184,11 @@ int main()
 		return -1;
 	}
 
-	// Window window(SCR_WIDTH, SCR_HEIGHT, "OpenGL");
-	// window.init();
+	glEnable(GL_DEPTH_TEST);
 
 	Shader ourShader("src/shaders/vertexShader.glsl", "src/shaders/fragmentShader.glsl");
 	
-	unsigned int VBO, VAO, EBO;
+	unsigned int VBO, VAO;
 	glGenVertexArrays(1, &VAO); // We can generate multiple VAOs or buffers at the same time glGenVertexArrays(1, VAOs);
 	glGenBuffers(1, &VBO);
 
@@ -150,17 +205,28 @@ int main()
 	glEnableVertexAttribArray(1);
 	
 	unsigned int texture1 = TextureLoader::loadAndGenerateTexture("C:\\Dev\\Alix_1.1\\textures\\container.jpg");   
-	unsigned int texture2 = TextureLoader::loadAndGenerateTexture("C:\\Dev\\Alix_1.1\\textures\\awesomeface.png");
+	//unsigned int texture2 = TextureLoader::loadAndGenerateTexture("C:\\Dev\\Alix_1.1\\textures\\awesomeface.png");
 	
 	ourShader.use();
 	ourShader.setInt("texture1", 0); 
-	ourShader.setInt("texture2", 1);
+	//ourShader.setInt("texture2", 1);
 
-	glEnable(GL_DEPTH_TEST);
+	// projection matrix
+	glm::mat4 projection;
+	projection = glm::perspective(glm::radians(fov), 800.0f / 600.0f, 0.1f, 100.0f);
+	ourShader.setMat4("projection", projection);
+
+	// ---------- To Do ----------
+	// Setting everything - classes in separate header files, with proper methods
 
 	// render loop
 	while (!glfwWindowShouldClose(window))
 	{
+		// pre-frame time logic
+		float currentFrame = static_cast<float>(glfwGetTime());
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
+
 		// input
 		processInput(window);
 
@@ -170,20 +236,17 @@ int main()
 
 		glActiveTexture(GL_TEXTURE0);
 		glBindTexture(GL_TEXTURE_2D, texture1);
-		glActiveTexture(GL_TEXTURE1);
-		glBindTexture(GL_TEXTURE_2D, texture2);
+		//glActiveTexture(GL_TEXTURE1);
+		//glBindTexture(GL_TEXTURE_2D, texture2);
 
-		ourShader.setFloat("mixValue", mixValue);
-
-		// view matrix
-		glm::mat4 view = glm::mat4(1.0f);
-		view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f)); // note that we're translating the scene in the reverse direction of where we want to move
+		// Camera
+		// ---------- To Do ----------
+		// Setting the camera as well as everything above - classes in separate header files, with proper methods
+		// Look at
+		glm::mat4 view;
+		view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
 		ourShader.setMat4("view", view);
-		// projection matrix
-		glm::mat4 projection;
-		projection = glm::perspective(glm::radians(90.0f), 800.0f / 600.0f, 0.1f, 100.0f);
-		ourShader.setMat4("projection", projection);
-
+		
 		glBindVertexArray(VAO);
 
 		for (unsigned int i = 0; i < 10; i++)
@@ -193,13 +256,11 @@ int main()
 			float angle = 20.0f * i;
 			if (i == 0)
 				angle = 240.0f;
-			model = glm::rotate(model, (float)glfwGetTime() * glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+			model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
 			ourShader.setMat4("model", model);
 
 			glDrawArrays(GL_TRIANGLES, 0, 36);
 		}
-		//glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-
 		// check and call events and swap the buffers
 		glfwSwapBuffers(window);
 		glfwPollEvents();
